07cea629d614663a816601aeb13a8b512c7f4814
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-11 16:47:32 +0100

    Añadiendo Getters y Setters

--- models/Vehiculo.php
+++ models/Vehiculo.php
@@ -22,4 +22,89 @@ class Vehiculo{
         return $precioTotal=$this->precioDia*$dias;
     }
 
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+    }
+
+    /**
+     * Get the value of marca
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set the value of marca
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+
+    }
+
+    /**
+     * Get the value of modelo
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * Set the value of modelo
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+
+    }
+
+    /**
+     * Get the value of matricula
+     */
+    public function getMatricula()
+    {
+        return $this->matricula;
+    }
+
+    /**
+     * Set the value of matricula
+     */
+    public function setMatricula($matricula)
+    {
+        $this->matricula = $matricula;
+
+    }
+
+    /**
+     * Get the value of precioDia
+     */
+    public function getPrecioDia()
+    {
+        return $this->precioDia;
+    }
+
+    /**
+     * Set the value of precioDia
+     */
+    public function setPrecioDia($precioDia)
+    {
+        $this->precioDia = $precioDia;
+
+    }
 }
