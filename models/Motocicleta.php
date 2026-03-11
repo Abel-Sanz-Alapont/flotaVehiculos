c11cf277d8cdc65e5893123deda14331c8f554ad
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-11 17:10:59 +0100

    Sobrescribiendo metodo de calcularAlquier:
    Si incluye casco añadir  +10€  al precio final

--- models/Motocicleta.php
+++ models/Motocicleta.php
@@ -3,12 +3,26 @@
 class Motocicleta extends Vehiculo{
 
     protected $cilindrada;
+    protected $incluyeCasco;
 
-    public function __construct($id, $marca, $modelo, $matricula, $precioDia,$cilindrada)
+    public function __construct($id, $marca, $modelo, $matricula, $precioDia,$cilindrada,$incluyeCasco)
     {
         parent::__construct($id, $marca, $modelo, $matricula, $precioDia);
         
         $this->cilindrada=$cilindrada;
+        $this->incluyeCasco=$incluyeCasco;
+    }
+
+    public function calcularAlquiler($dias)
+    {
+        $precioFinal= parent::calcularAlquiler($dias);
+
+        $this->incluyeCasco=true;
+
+        if($this->incluyeCasco){
+            $precioFinal= $precioFinal+10;
+        }
+        return $precioFinal;
     }
 
 }
