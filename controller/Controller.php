5a3634e9a31298d378e7356484b39a5e4f430ad8
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-13 16:01:31 +0100

    Añadiendo metodo agregaCoches

--- controller/Controller.php
+++ controller/Controller.php
@@ -10,8 +10,29 @@ class Controller
     }
     public function index()
     {
-        $arrayCoches = $this->gestor->listarCoche(); //devuelve array de objetos
+        $arrayCoches = $this->gestor->listarCoches(); //devuelve array de objetos
         include "views/listar.php";
     }
+    public function agregarCoches()
+    {
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  
+                $marca=$_POST['marca'];
+                $modelo=$_POST['modelo'];
+                $matricula=$_POST['matricula'];
+                $precioDia=$_POST['precioDia'];
+                $numeroPuertas=$_POST['numeroPuertas'];
+                $tipoCombustible=$_POST['tipoCombustible'];
+
+            $nuevoCoche = new Persona(null,$marca,$modelo,$matricula,$precioDia,$numeroPuertas,$tipoCombustible);
+            $this->gestor->agregar($nuevoCoche);
+
+            header("Location: index.php");
+            exit;
+        }
+
+
+        include "views/agregar.php";
+    }
 }
