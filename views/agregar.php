ff5eb3b7f0089c7f6322a6dce481324aa7a94496
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-13 15:27:29 +0100

    Formulario para agregar Coches

---
+++ views/agregar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Agregar Vehiculos</title>
+</head>
+
+<body>
+    <h1>Agregar Coche</h1>
+    <form method="POST">
+        Nombre:<br>
+        <input type="text" name="marca" require><br>
+        <input type="text" name="modelo" require><br>
+        <input type="text" name="matricula" require><br>
+        <input type="number" name="precioDia" require><br>
+        <input type="number" name="numeroPuertas" require><br>
+        <input type="number" name="tipoCombustible" require><br>
+
+        <button type="submit">Agregar Coche</button>
+    </form>
+    <br>
+
+    <!--<h1>Agregar Motocicleta</h1>
+    <form method="POST">
+        Nombre:<br>
+        <input type="text" name="nombre" require><br>
+
+        <button type="submit">Agregar Motocicleta</button>
+    </form>
+    <br>-->
+
+    <a href="index.php">Volver a Index</a>
+
+
+
+</body>
+
+</html>
