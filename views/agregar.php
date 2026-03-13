f20d95feda6e3e08b9cc81474393f507818955d4
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-13 16:28:29 +0100

    Agrerando placeholder al codigo del form

--- views/agregar.php
+++ views/agregar.php
@@ -8,13 +8,13 @@
 <body>
     <h1>Agregar Coches</h1>
     <form method="POST">
-        Nombre:<br>
-        <input type="text" name="marca" require><br>
-        <input type="text" name="modelo" require><br>
-        <input type="text" name="matricula" require><br>
-        <input type="number" name="precioDia" require><br>
-        <input type="number" name="numeroPuertas" require><br>
-        <input type="number" name="tipoCombustible" require><br>
+        <input type="text" name="marca" placeholder="marca" require><br>
+        <input type="text" name="modelo" placeholder="modelo" require><br>
+        <input type="text" name="matricula" placeholder="matricula" require><br>
+        <input type="number" name="precioDia" placeholder="Precio" require><br>
+        <input type="number" name="numeroPuertas" placeholder="Numero Puertas" require><br>
+        <input type="text" name="tipoCombustible" placeholder="Combustible" require><br>
+        <br>
 
         <button type="submit">Agregar Coche</button>
     </form>
