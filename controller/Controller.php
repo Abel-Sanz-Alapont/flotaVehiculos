6fb827cbb64bbccb3fa27d5d48206b36aced1d5c
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-14 18:31:23 +0100

    Corrigiendo Errores al llamar las funciones con el gestor y al crear una nueva Motocicleta no llamar a la clase Motocicleta

--- controller/Controller.php
+++ controller/Controller.php
@@ -10,33 +10,34 @@ class Controller
     }
     public function index()
     {
-        $arrayCoches = $this->gestor->listarCoches(); //devuelve array de objetos
+        $arrayVehiculos = $this->gestor->listarVehiculos(); //devuelve array de objetos
         include "views/listar.php";
     }
 
-    public function tipoVehiculo(){
-         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $tipo=$_POST['tipoVehiculo'];
-            if ($tipo =='Coche') {
+    public function tipoVehiculo()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $tipo = $_POST['tipoVehiculo'];
+            if ($tipo == 'Coche') {
                 $this->agregarCoches();
-            }elseif($tipo=='Motocicleta'){
+            } elseif ($tipo == 'Motocicleta') {
                 $this->agregarMotocicletas();
             }
-         }
+        }
     }
     public function agregarCoches()
     {
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  
-                $marca=$_POST['marca'];
-                $modelo=$_POST['modelo'];
-                $matricula=$_POST['matricula'];
-                $precioDia=$_POST['precioDia'];
-                $numeroPuertas=$_POST['numeroPuertas'];
-                $tipoCombustible=$_POST['tipoCombustible'];
 
-            $nuevoCoche = new Coche(null,$marca,$modelo,$matricula,$precioDia,$numeroPuertas,$tipoCombustible);
+            $marca = $_POST['marca'];
+            $modelo = $_POST['modelo'];
+            $matricula = $_POST['matricula'];
+            $precioDia = $_POST['precioDia'];
+            $numeroPuertas = $_POST['numeroPuertas'];
+            $tipoCombustible = $_POST['tipoCombustible'];
+
+            $nuevoCoche = new Coche(null, $marca, $modelo, $matricula, $precioDia, $numeroPuertas, $tipoCombustible);
             $this->gestor->agregarCoches($nuevoCoche);
 
             header("Location: index.php");
@@ -47,22 +48,22 @@ class Controller
         include "views/agregar.php";
     }
 
-        public function agregarMotocicletas()
+    public function agregarMotocicletas()
     {
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  
-                $marca=$_POST['marca'];
-                $modelo=$_POST['modelo'];
-                $matricula=$_POST['matricula'];
-                $precioDia=$_POST['precioDia'];
-                $cilindrada=$_POST['cilindrada'];
-                $incluyeCasco=0;
-                if (isset($_POST['incluyeCasco'])) {
-                    $incluyeCasco=1;
-                }
 
-            $nuevaMotocicleta = new Coche(null,$marca,$modelo,$matricula,$precioDia,$cilindrada,$incluyeCasco);
+            $marca = $_POST['marca'];
+            $modelo = $_POST['modelo'];
+            $matricula = $_POST['matricula'];
+            $precioDia = $_POST['precioDia'];
+            $cilindrada = $_POST['cilindrada'];
+            $incluyeCasco = 0;
+            if (isset($_POST['incluyeCasco'])) {
+                $incluyeCasco = 1;
+            }
+
+            $nuevaMotocicleta = new Motocicleta(null, $marca, $modelo, $matricula, $precioDia, $cilindrada, $incluyeCasco);
             $this->gestor->agregarMotocicletas($nuevaMotocicleta);
 
             header("Location: index.php");
@@ -72,16 +73,16 @@ class Controller
 
         include "views/agregar.php";
     }
-       public function editarCoches()
+    public function editarCoches()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-           $this->gestor->editarCoches($_POST['id'], $_POST['marca'], $_POST['modelo'], $_POST['matricula'], $_POST['precioDia'], $_POST['numeroPuertas'], $_POST['tipoCombustible']);
+            $this->gestor->editarCoches($_POST['id'], $_POST['marca'], $_POST['modelo'], $_POST['matricula'], $_POST['precioDia'], $_POST['numeroPuertas'], $_POST['tipoCombustible']);
 
             header("Location: index.php");
             exit;
         }
     }
-        public function eliminar()
+    public function eliminar()
     {
         $id = $_GET['id'];
         $this->gestor->eliminar($id);
