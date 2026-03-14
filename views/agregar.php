566bd2687a66c38fa5d1174e356e52adbb2e54e8
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-03-14 18:33:26 +0100

    Reformulando el formulario para poder añadir motocicletas

--- views/agregar.php
+++ views/agregar.php
@@ -14,47 +14,38 @@
 
 <body>
     <main>
-        <h1>Formulario para Agregar Vehiculos(Coches/Motocicletas)</h1><a href="index.php">Volver a Index</a>
+        <h1>Formulario para Agregar Vehiculos(Coches/Motocicletas)</h1>
+        <a href="index.php">Volver a Index</a>
         <hr>
     </main>
     <nav>
+        <form method="POST" action="index.php?accion=guardarVehiculo">
 
-
-        <h1>Agregar Coches</h1>
-
-        <form method="POST">
+            <h1> Selecciona el Tipo de Vehiculo</h1>
+            <select name="tipoVehiculo" require>
+                <option value="Coche">Coche</option>
+                <option value="Motocicleta">Motocicleta</option>
+            </select><br>
+            <h3>Datos Generales</h3>
             <input type="text" name="marca" placeholder="marca" require><br>
             <input type="text" name="modelo" placeholder="modelo" require><br>
             <input type="text" name="matricula" placeholder="matricula" require><br>
             <input type="number" name="precioDia" placeholder="Precio" require><br>
+
+            <h3>Datos solo para Coches</h3>
             <input type="number" name="numeroPuertas" placeholder="Numero Puertas" require><br>
             <input type="text" name="tipoCombustible" placeholder="Combustible" require><br>
             <br>
 
+            <h3>Datos solo para Motos</h3>
+            <input type="text" name="cilindrada" placeholder="Cilindrada" require><br>
+            incluye Casco:<input type="checkbox" name="incluyeCasco" placeholder="incluyeCasco" value="1" require><br>
+
             <button type="submit">Agregar Coche</button>
         </form>
-
-
-
-        <h1>Agregar Motocicletas</h1>
-
-        <input type="text" name="marca" placeholder="marca" require><br>
-        <input type="text" name="modelo" placeholder="modelo" require><br>
-        <input type="text" name="matricula" placeholder="matricula" require><br>
-        <input type="number" name="precioDia" placeholder="Precio" require><br>
-        <input type="number" name="casco" placeholder="Casco" require><br>
-        <input type="text" name="cilindrada" placeholder="Cilindrada" require><br>
-        <br>
-
-        <button type="submit">Agregar Motocicleta</button>
-        </form>
-        <br>
-
-        <form method="POST">
-
     </nav>
     <br>
-    
+
 </body>
 
 </html>
